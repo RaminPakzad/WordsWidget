b28e1df93806c12c3357f7703a2e9caf74f0a642
Ramin <ramin.highborn@yahoo.com>
2019-04-04 11:47:15 +0430

    remove list view and use textview

--- app/src/main/java/words/learn/com/wordswidgetandroid/WordsWidget.java
+++ app/src/main/java/words/learn/com/wordswidgetandroid/WordsWidget.java
@@ -3,8 +3,6 @@ package words.learn.com.wordswidgetandroid;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.widget.RemoteViews;
 
 /**
@@ -12,39 +10,37 @@ import android.widget.RemoteViews;
  */
 public class WordsWidget extends AppWidgetProvider {
 
-    public WordsWidget() {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+        views.setTextViewText(R.id.key1, "key1x");
+        views.setTextViewText(R.id.key2, "key2x");
+        views.setTextViewText(R.id.value1, "value1x");
+        views.setTextViewText(R.id.value2, "value2x");
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        int N = appWidgetIds.length;
-
-        for (int i = 0; i < N; ++i) {
-            RemoteViews remoteViews = this.updateWidgetListView(context, appWidgetIds[i]);
-            appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
-    private RemoteViews updateWidgetListView(Context context, int appWidgetId) {
-        //which layout to show on widget
-        RemoteViews remoteViews = new RemoteViews(
-                context.getPackageName(),R.layout.widget_layout);
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
 
-        //RemoteViews Service needed to provide adapter for ListView
-        Intent svcIntent = new Intent(context, WidgetService.class);
-        //passing app widget id to that RemoteViews Service
-        svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        //setting a unique Uri to the intent
-        //don't know its purpose to me right now
-        svcIntent.setData(Uri.parse(
-                svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
-        //setting adapter to listview of the widget
-        remoteViews.setRemoteAdapter(appWidgetId, R.id.listViewWidget,
-                svcIntent);
-        //setting an empty view in case of no data
-        remoteViews.setEmptyView(R.id.listViewWidget, R.id.empty_view);
-        return remoteViews;
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
     }
 }
 
